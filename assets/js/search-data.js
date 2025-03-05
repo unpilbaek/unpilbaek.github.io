0a25a20d04312208f8980163c477e5953ed341a4
unpilbaek <unpilbaek@gmail.com>
2025-03-05 09:37:56 +0000

    Deploying to gh-pages from @ unpilbaek/unpilbaek.github.io@87e48b841c35eb9d342cc8b5a4f7088da97c1bb1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,18 +59,7 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-hello-world-wave",
+    },{id: "news-hello-world-wave",
           title: 'Hello world! :wave:',
           description: "",
           section: "News",},{id: "projects-project-1",
