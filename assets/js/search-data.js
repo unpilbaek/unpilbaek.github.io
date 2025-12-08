e8864cadf35c9cb37476ee49c111e65d1cd96545
unpilbaek <unpilbaek@gmail.com>
2025-12-08 19:38:42 +0000

    Deploying to gh-pages from @ unpilbaek/unpilbaek.github.io@49b997cfeb6cdf55602daf3c5b89a0d325aec1c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -91,6 +91,13 @@ ninja.data = [{
           window.open("https://instagram.com/unpil.baek", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/unpilbaek", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
